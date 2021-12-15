2c4ddf86dc02a3de9640c264f8cbfb2cd6d750fd
skynalix <skynalix@gmail.com>
2021-12-15 19:52:48 +0100

    On peut maintenant save en PNG

--- src/main/java/edu/moria/moriaale/controllers/Drawer.java
+++ src/main/java/edu/moria/moriaale/controllers/Drawer.java
@@ -5,6 +5,7 @@ import edu.moria.moriaale.Complexe;
 import edu.moria.moriaale.InputMenu;
 import edu.moria.moriaale.Utils;
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.image.ImageView;
@@ -12,8 +13,12 @@ import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -30,12 +35,13 @@ public class Drawer {
 	public double MOVE_X = 0.1;
 	public double MOVE_Y = 0.1;
 	public InputMenu.Inputs inputs;
+	private WritableImage buffer;
 
 
 	public void initialize() {
 		Drawer.instance = this;
 		inputs = InputMenu.showDialog();
-		if( inputs == null )Platform.runLater( () -> App.transferTo( Utils.GUI.MAIN_MENU ) );
+		if( inputs == null ) Platform.runLater( () -> App.transferTo( Utils.GUI.MAIN_MENU ) );
 		draw();
 		refreshButtonsPosition();
 	}
@@ -51,7 +57,12 @@ public class Drawer {
 		}
 		drawingPane = new BorderPane();
 		gamePane.getChildren().add( drawingPane );
-		WritableImage buffer = new WritableImage( inputs.maxWidth, inputs.maxHeight );
+
+		if( inputs == null ) {
+			Platform.runLater( () -> App.transferTo( Utils.GUI.MAIN_MENU ) );
+			return;
+		}
+		buffer = new WritableImage( inputs.maxWidth, inputs.maxHeight );
 		PixelWriter pw = buffer.getPixelWriter();
 
 		int nbBlock = 4;
@@ -62,6 +73,31 @@ public class Drawer {
 			Platform.runLater( generator );
 		}
 		Drawer.instance.drawingPane.getChildren().add( new ImageView( buffer ) );
+
+	}
+
+	@FXML
+	private void onToImagePressed() {
+
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().add( new FileChooser.ExtensionFilter( "PNG", "*.png" ) );
+
+		File file = fileChooser.showSaveDialog( App.mainInstance.primaryStage );
+
+		if( file != null ) {
+			String fileName = file.getName();
+
+			if( !fileName.toUpperCase().endsWith( ".PNG" ) ) {
+				file = new File( file.getAbsolutePath() + ".png" );
+			}
+			try {
+				ImageIO.write( SwingFXUtils.fromFXImage( buffer, null ),
+							   "png", file );
+			} catch( IOException e ) {
+				e.printStackTrace();
+				System.out.println( "An error occurred while trying to save the image" );
+			}
+		}
 
 	}
 
@@ -111,7 +147,7 @@ public class Drawer {
 	}
 
 	public void refreshButtonsPosition() {
-		buttonBar.setLayoutX( ( App.mainInstance.primaryStage.getWidth() - buttonBar.getWidth() ) / 2 - 155 );
+		buttonBar.setLayoutX( ( App.mainInstance.primaryStage.getWidth() - buttonBar.getWidth() ) / 2 - 195 );
 		buttonBar.setLayoutY( App.mainInstance.primaryStage.getHeight() - ( 2 * buttonBar.getHeight() ) - 110 );
 	}
 
